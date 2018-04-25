6fd22bb2a8fe67fd76cb2a193c5bad986799c37b
rajukc47 <raju.kc3@gmail.com>
2018-04-25 11:32:24 +0545

    minor changes in jobs.php file

--- jobpagination.php
+++ jobpagination.php
@@ -30,7 +30,7 @@ if($result->num_rows > 0) {
               <div class="attachment-pushed">
                 <h4 class="attachment-heading"><a href="view-job-post.php?id=<?php echo $row['id_jobpost']; ?>"><?php echo $row['jobtitle']; ?></a> <span class="attachment-heading pull-right">NRs <?php echo $row['maximumsalary']; ?>/Month</span></h4>
                 <div class="attachment-text">
-                    <div><strong><?php echo $row1['companyname']; ?> | <?php echo $row1['city']; ?> | Experience <?php echo $row['experience']; ?> Years</strong></div>
+                    <div><strong><?php echo $row1['companyname']; ?> | <?php echo $row1['city']; ?> | Experience <?php echo $row['experience']; ?> Years| Closing Date : <?php echo $row['closingdate']; ?></strong></div>
                 </div>
               </div>
             </div>
